9c9a37ced64921b73945b2b880034ed942f6547b
CSPaulia <cspaulanda@outlook.com>
2023-11-22 14:42:26 +0800

    first commit

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ eos_idx = 3
 src_embedding_size = 512
 tgt_embedding_size = 512
 
-batch_size = 256
+batch_size = 128
 epoch_num = 40
 early_stop = 5
 lr = 3e-4
@@ -28,13 +28,13 @@ max_len = 60
 beam_size = 3
 
 data_dir = './data'
-train_data_path = '/root/GenGM/data/midiAll/train'
-dev_data_path = '/root/GenGM/data/midiAll/validate'
-test_data_path = '/root/GenGM/data/midiAll/test'
+train_data_path = '/mnt/data/repos/GenGM/data/midiAll/train'
+dev_data_path = '/mnt/data/repos/GenGM/data/midiAll/validate'
+test_data_path = '/mnt/data/repos/GenGM/data/midiAll/test'
 model_path = './experiment'
 log_path = './experiment/train.log'
 output_path = './experiment/output.txt'
-label_mapping = '/root/GenGM/utils/DrumMusic.yaml'
+label_mapping = '/mnt/data/repos/GenGM/utils/DrumMusic.yaml'
 
 # gpu_id and device id is the relative id
 # thus, if you wanna use os.environ['CUDA_VISIBLE_DEVICES'] = '2, 3'
